5114ff590e850c7b2b5b65897db0f490375d0181
Павел <taxa3008@gmail.com>
2020-04-04 01:22:16 +0300

    Добавлены 2 класса с локатарами MainPageLocators b LoginPageLocators

---
+++ pages/locators.py
@@ -0,0 +1,13 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class LoginPageLocators():
+    LOGIN_USERNAME = (By.NAME, "login-username")
+    LOGIN_PASSWORD = (By.NAME, "login-password")
+    REGISTER_EMAIL = (By.NAME, "registration-email")
+    REGISTER_PASSWORD1 = (By.NAME, "registration-password1")
+    REGISTER_PASSWORD2 = (By.NAME, "registration-password2")
