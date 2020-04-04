7d20fa8b092555c969d3ff95c584c97577e87f3e
Павел <taxa3008@gmail.com>
2020-04-05 02:42:25 +0300

    Всё посмотрел, всё поправил.

--- conftest.py
+++ conftest.py
@@ -13,7 +13,8 @@ def browser(request):
     print("\nstart browser for test..")
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': language})
-    browser = webdriver.Chrome(options=options)
+    browser = webdriver.Chrome('C:\\chromedriver\\chromedriver.exe', options=options)
+    # browser = webdriver.Chrome(options=options)
     yield browser
     print("\nquit browser..")
     browser.quit()
